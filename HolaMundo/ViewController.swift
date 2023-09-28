9ef00dacfe7096cf1e0167e27692309eaaa73075
lluissubirats <lluis.subiratsballesteros@cieep.com>
2023-09-28 13:12:15 +0200

    commit2

--- HolaMundo/ViewController.swift
+++ HolaMundo/ViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { //es el equivalente a Main Activity de android
 
-    override func viewDidLoad() {
+    @IBOutlet weak var lbTexto: UILabel!
+    
+    override func viewDidLoad() { //equivalente a onCreate
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func escribeSaludo(_ sender: Any) {
+        
+        lbTexto.text = "HOLA MUNDO"
+    }
+    
 }
 
